5ef21a5cb83d8a48e5d86fa67643aa5f1f903b47
TaxiService <serviceoftaxi@gmail.com>
2025-10-01 14:03:28 +0200

    fixed leap bang

--- seasoning/plugins/left.d/narrative-1st.sh
+++ seasoning/plugins/left.d/narrative-1st.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail; LC_TIME=C
-d="$(date +%d)"; m="$(date +%m)"; dow="$(date +%A|tr '[:upper:]' '[:lower:]')"; h=$((10#$(date +%H)))
-case "$h" in 0|1|2|3|4|5) tod="night";; 6|7|8|9|10|11) tod="morning";; 12|13|14|15|16|17) tod="afternoon";; *) tod="evening";; esac
-n=$(( (10#$d-1)/7 + 1 )); case "$n" in 1)s="st";;2)s="nd";;3)s="rd";;*)s="th";; esac; excl=""; [[ "$m"=="02" && "$d"=="29" ]]&&excl="!"
-printf "%d%s %s %s%s\n" "$n" "$s" "$dow" "$tod" "$excl"
+set -o pipefail
+m=$(date +%-m); d=$(date +%-d)
+if [ "$m" -eq 2 ] && [ "$d" -eq 29 ]; then LEAP_BANG="!"; else LEAP_BANG=""; fi
+dow="$(date +%A|tr '[:upper:]' '[:lower:]')";
+h=$((10#$(date +%H)))
+case "$h" in  0| 1| 2| 3| 4| 5) tod="night";;
+			  6| 7| 8| 9|10|11) tod="morning";;
+			 12|13|14|15|16|17) tod="afternoon";;
+			 *) tod="evening";;
+esac
+n=$(( (10#$d-1)/7 + 1 ));
+case "$n" in 1)s="st";;
+			 2)s="nd";;
+			 3)s="rd";;
+			 *)s="th";;
+esac
+printf "%d%s %s %s%s\n" "$n" "$s" "$dow" "$tod" "$LEAP_BANG"
